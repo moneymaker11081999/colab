21c180dd2e406af9c287e745836e94edaf057570
AnhTN <cachepdibao13@gmail.com>
2021-02-19 14:02:29 +0700

    ok

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  mailer: {
+    service: 'Gmail',
+    auth: {
+      user: 'cachepdibao13@gmail.com',
+      pass: 'Roadtoti6'
+    }
+  },
+  dbConnstring: 'mongodb+srv://cachepdibao13:Roadtoti6@cluster0.hmdao.mongodb.net/OnlineCollabarativeEditor?retryWrites=true&w=majority',
+  sessionKey: 'HaloCode4Share'
+}
